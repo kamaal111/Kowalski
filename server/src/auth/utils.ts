6c91d7789915884d8844de06edd934974d7f7d56
Kamaal Farah <kamaal.f1@gmail.com>
2025-09-07 20:24:23 +0200

    Removing auth spec
    Signed-off-by: Kamaal Farah <kamaal.f1@gmail.com>

--- server/src/auth/utils.ts
+++ server/src/auth/utils.ts
@@ -1,20 +1,14 @@
-import type z from 'zod';
+import z from 'zod';
 
 import type { HonoContext } from '../api/contexts.js';
 import { makeNewRequest } from '../utils/request.js';
 import { BetterAuthException } from './exceptions.js';
 import type { BODY_TYPES } from '../constants/request.js';
 
-function isBetterAuthException(response: unknown): response is { code: string; message: string } {
-  return (
-    response != null &&
-    typeof response === 'object' &&
-    'code' in response &&
-    'message' in response &&
-    typeof response.code === 'string' &&
-    typeof response.message === 'string'
-  );
-}
+const BetterAuthExceptionSchema = z.object({
+  code: z.string(),
+  message: z.string(),
+});
 
 export async function handleAuthRequest<Schema extends z.ZodType>(
   c: HonoContext,
@@ -23,10 +17,11 @@ export async function handleAuthRequest<Schema extends z.ZodType>(
   const request = await makeNewRequest(c, options);
   const response = await c.get('auth').handler(request);
   const jsonResponse: unknown = await response.json();
-  if (isBetterAuthException(jsonResponse)) {
+  const exceptionResult = await BetterAuthExceptionSchema.safeParseAsync(jsonResponse);
+  if (exceptionResult.success) {
     throw new BetterAuthException(c, {
-      code: jsonResponse.code,
-      message: jsonResponse.message,
+      code: exceptionResult.data.code,
+      message: exceptionResult.data.message,
       headers: response.headers,
     });
   }
